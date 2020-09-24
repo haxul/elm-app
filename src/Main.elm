61f4f6d5919d5bf7066aedaa967240ce421d21c9
haxul <s.s.starodubov@yandex.ru>
2020-09-24 16:49:07 +0400

    review

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,25 @@
 module Main exposing (main)
 
-import Debug exposing (log)
-import Html exposing (text)
+import Html exposing (div, h1, h2, li, text, ul)
+import Html.Attributes exposing (class, id)
 
+pluralize singular plural amount =
+    if amount == 1 then
+       singular
+     else plural
 
-main = text ("hi" )
+languages = ul [class "langs" ]
+    [ li [] [ text "js"]
+     ,li [] [ text "ts" ]
+     ,li [] [ text  "elm" ]
+    ]
+banner = h2 [class "banner"] [text "nice banner"]
+
+result = div []
+    [
+     h1 [id "header"] [ text "Application"]
+     , languages
+     , banner
+    ]
+
+main = result
